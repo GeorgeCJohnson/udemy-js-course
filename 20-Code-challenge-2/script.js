e40c0b2954017b1b32742823960e7cd3c7a65ba4
George Johnson <gjohnson.pioneer@gmail.com>
2023-02-27 18:09:28 -0500

    Complete lesson 22.

--- 20-Code-challenge-2/script.js
+++ 20-Code-challenge-2/script.js
@@ -3,26 +3,26 @@
 // const johnMass= 92;
 // const johnHeight= 1.95;
 
-const markMass= 60;
-const markHeight= 1.69;
-const johnMass= 92;
-const johnHeight= 1.95;
+// const markMass= 60;
+// const markHeight= 1.69;
+// const johnMass= 92;
+// const johnHeight= 1.95;
 
-const markBmi=markMass / markHeight ** 2;
-console.log(markBmi)
+// const markBmi=markMass / markHeight ** 2;
+// console.log(markBmi)
 
-const johnBmi=johnMass / johnHeight ** 2;
-console.log(johnBmi)
+// const johnBmi=johnMass / johnHeight ** 2;
+// console.log(johnBmi)
 
-const markHigherBmi=markBmi>johnBmi
-console.log(markHigherBmi)
+// const markHigherBmi=markBmi>johnBmi
+// console.log(markHigherBmi)
 
 
-if (markBmi > johnBmi) {
-    console.log(`Mark's BMI (${markBmi}) is higher than Johns's! ${johnBmi})`);
-} else {
-    console.log(`John's BMI (${johnBmi}) is higher than Mark's (${markBmi}`);
-}
+// if (markBmi > johnBmi) {
+//     console.log(`Mark's BMI (${markBmi}) is higher than Johns's! ${johnBmi})`);
+// } else {
+//     console.log(`John's BMI (${johnBmi}) is higher than Mark's (${markBmi}`);
+// }
 
 // let task1='9' - 5;
 // console.log(task1);
@@ -37,3 +37,15 @@ if (markBmi > johnBmi) {
 
 // let task5=5 + 6 + '4' + 9 - 4 - 2;
 // console.log(task5);
+
+const numNeighbors = Number(prompt('How many borders does your country have?'));
+
+if (numNeighbors === 1) { 
+console.log('Only 1 border!'); 
+} else if(numNeighbors > 1) { 
+    console.log("More than one border");
+} else {
+    console.log("No border ?!");
+}
+    
+    
