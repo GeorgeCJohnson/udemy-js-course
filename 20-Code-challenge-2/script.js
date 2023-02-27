6876720a9a2137909409239a3e9442178cff2800
George Johnson <gjohnson.pioneer@gmail.com>
2023-02-27 16:10:14 -0500

    Complete lesson 20.

--- 20-Code-challenge-2/script.js
+++ 20-Code-challenge-2/script.js
@@ -1,25 +1,39 @@
-// const markMass= 78;
+// // const markMass= 78;
+// // const markHeight= 1.69;
+// // const johnMass= 92;
+// // const johnHeight= 1.95;
+
+// const markMass= 60;
 // const markHeight= 1.69;
 // const johnMass= 92;
 // const johnHeight= 1.95;
 
-const markMass= 60;
-const markHeight= 1.69;
-const johnMass= 92;
-const johnHeight= 1.95;
+// const markBmi=markMass / markHeight ** 2;
+// console.log(markBmi)
 
-const markBmi=markMass / markHeight ** 2;
-console.log(markBmi)
+// const johnBmi=johnMass / johnHeight ** 2;
+// console.log(johnBmi)
 
-const johnBmi=johnMass / johnHeight ** 2;
-console.log(johnBmi)
-
-const markHigherBmi=markBmi>johnBmi
-console.log(markHigherBmi)
+// const markHigherBmi=markBmi>johnBmi
+// console.log(markHigherBmi)
 
 
-if (markBmi > johnBmi) {
-    console.log(`Mark's BMI (${markBmi}) is higher than Johns's! ${johnBmi})`);
-} else {
-    console.log(`John's BMI (${johnBmi}) is higher than Mark's (${markBmi}`);
-}
+// if (markBmi > johnBmi) {
+//     console.log(`Mark's BMI (${markBmi}) is higher than Johns's! ${johnBmi})`);
+// } else {
+//     console.log(`John's BMI (${johnBmi}) is higher than Mark's (${markBmi}`);
+// }
+
+// let task1='9' - 5;
+// console.log(task1);
+
+// let task2='19' - '13' + '17';
+
+// let task3='19' - '13' + 17;
+// console.log(task3);
+
+// let task4='123' < 57;
+// console.log(task4);
+
+// let task5=5 + 6 + '4' + 9 - 4 - 2;
+// console.log(task5);
