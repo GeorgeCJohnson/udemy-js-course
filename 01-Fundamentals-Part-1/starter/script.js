bbfbd4b71ed7a3ddfde044583a037550c5a57446
George Johnson <gjohnson.pioneer@gmail.com>
2023-02-27 11:54:17 -0500

    Copmlete lesson 14.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,10 +1,22 @@
 const country="United States";
 let continent="North America";
-let population="5,000,000,000";
+let population=1005;
 const isIsland="false";
 let language="English";
+let finland=500
+let avgCountry=250
+const description1=
+country + " is in "
++ continent + " which has a population of "
++ population + " and it's 1005 people speak " + language;
 
 console.log (typeof isIsland);
 console.log (typeof population);
 console.log (typeof country);
 console.log (typeof language);
+
+// console.log(population/2)
+console.log (population>finland)
+console.log (avgCountry>population)
+
+console.log (description1)
