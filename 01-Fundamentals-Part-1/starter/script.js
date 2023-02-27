5790f533344d47bf2db279306fcb36946722c977
George Johnson <gjohnson.pioneer@gmail.com>
2023-02-27 13:16:06 -0500

    Add coding challenges.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -5,18 +5,18 @@ const isIsland="false";
 let language="English";
 let finland=500
 let avgCountry=250
-const description1=
-country + " is in "
-+ continent + " which has a population of "
-+ population + " and it's 1005 people speak " + language;
 
 console.log (typeof isIsland);
 console.log (typeof population);
 console.log (typeof country);
 console.log (typeof language);
-
-// console.log(population/2)
+console.log(population/2)
 console.log (population>finland)
 console.log (avgCountry>population)
+
+const description1= "The " +
+country + " is in "
++ continent + " which has a population of "
++ population + " and it's 1005 people speak " + language;
 
 console.log (description1)
