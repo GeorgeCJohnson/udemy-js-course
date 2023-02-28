b14eb436a4dada2fb7386f312a0b30e35f8c6e1e
George Johnson <gjohnson.pioneer@gmail.com>
2023-02-28 13:55:09 -0500

    Complete lesson 28.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -46,25 +46,31 @@
 // } else {
 //     console.log("other");
 // }
-const language="Portugese"
+// const language="Portugese"
 
-switch(language) {
-    case "Chinese": 
-    case "Mandarin":
-        console.log("MOST number of native speakers");
-        break;
-    case "Spanish":
-        console.log("2nd place in number of native speakers");
-        break;
-    case "English": 
-        console.log("3rd place");
-        break;
-    case "Hindi":
-        console.log("Number 4");
-        break;
-    case "arabic":
-        console.log("5th most spoken language");
-        break;
-        default:
-            console.log("Great language too :D");
-}        
+// switch(language) {
+//     case "Chinese": 
+//     case "Mandarin":
+//         console.log("MOST number of native speakers");
+//         break;
+//     case "Spanish":
+//         console.log("2nd place in number of native speakers");
+//         break;
+//     case "English": 
+//         console.log("3rd place");
+//         break;
+//     case "Hindi":
+//         console.log("Number 4");
+//         break;
+//     case "arabic":
+//         console.log("5th most spoken language");
+//         break;
+//         default:
+//             console.log("Great language too :D");
+// }  
+const country= "Portgual"       
+const population= "130";
+
+console.log(
+`${country}'s population is ${population > 33 ? "above" : "below"} average`,
+);
