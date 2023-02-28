510ad71097482ff1b96a23c893da2658af1958e6
George Johnson <gjohnson.pioneer@gmail.com>
2023-02-27 21:10:47 -0500

    Complete coding challenge 3.

---
+++ 21-Coding-challenge-3/script.js
@@ -0,0 +1,10 @@
+let dolphinsScore = Number(96, 108, 89);
+console.log()
+let koalasScore = Number(88, 91, 110);
+
+if (dolphinsScore / 3 > koalasScore / 3) 
+
+{console.log("Dolphins win!");
+} else {
+    console.log("Koala's win!!");
+}
