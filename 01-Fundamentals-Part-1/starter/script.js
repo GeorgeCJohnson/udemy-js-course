158b8aafd793382d2aed49ae0883ce11de8392f4
George Johnson <gjohnson.pioneer@gmail.com>
2023-02-27 20:12:21 -0500

    Add answer from instructor as option.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,10 +1,10 @@
-// const country="United States";
-// let continent="North America";
-// let population=1005;
-// const isIsland="false";
-// let language="English";
-// let finland=500
-// let avgCountry=250
+const country="United States";
+let continent="North America";
+let population=1005;
+const isIsland="false";
+let language="English";
+let finland=500
+let avgCountry=250
 
 // console.log (typeof isIsland);
 // console.log (typeof population);
@@ -17,10 +17,19 @@
 // const description1= `The ${country} is in ${continent}, and its ${population} people speak ${language}`;
 
 // console.log (description1)
-const languagePref = true;
-const popPref = false;
-const landType = false;
+// const languagePref = true;
+// const popPref = false;
+// const landType = false;
 
-if (languagePref && popPref && landType) 
-    {console.log('Yes! Sarah should move to America');
-} else (console.log('Sarah should run away!!!'))
+// if (languagePref && popPref && landType) 
+//     {console.log('Yes! Sarah should move to America');
+// } else (console.log('Sarah should run away!!!'))
+
+// OR the following which was the answer provided//
+
+if (language === 'English' && !population < 50 && isIsland)
+{
+    console.log(`Sarah should live in ${country}`);
+} else {
+    console.log(`${country} Sarah should run!`);
+}
