10441de78a9c2a441aa635ff1657bf3327454055
George Johnson <gjohnson.pioneer@gmail.com>
2023-02-27 21:40:40 -0500

    Add bonus material.

--- 21-Coding-challenge-3/script.js
+++ 21-Coding-challenge-3/script.js
@@ -1,16 +1,11 @@
-let dolphinsScore = ((96 + 108 + 89) /3);
-
-let koalasScore = ((96 + 108 + 89) / 3);
-
-if (dolphinsScore > koalasScore) 
-
-{console.log("Dolphins win!");
-} 
-else if (koalasScore / 3 > dolphinsScore / 3)
-{
-    console.log("Koala's win!!");
-} else
-
-{
+let dolphinsScore = ((96 + 108 + 55) /3);
+let koalasScore = ((96 + 108 + 78) / 3);
+console.log(dolphinsScore, koalasScore)
+if (dolphinsScore > koalasScore && dolphinsScore >= 100){
+    console.log("Dolphins win! 🏆");
+} else if (koalasScore > dolphinsScore && koalasScore >= 100){
+    console.log("Koala's win!!🏆");
+} else if (dolphinsScore === koalasScore && dolphinsScore >= 100 && koalasScore >= 100){
     console.log("It's a draw!")
 }
+ else {console.log('There is no winner');}   
