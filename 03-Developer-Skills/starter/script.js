0f4f2aebb5e64b303ffd715247dbe579e2a16a6f
George Johnson <gjohnson.pioneer@gmail.com>
2023-03-02 12:33:03 -0500

    Add first and second arrays.

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,19 +1,26 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const percentageOfWorld1 = function (populations) {
-    return (populations / 7900) * 100;
-}
+// const percentageOfWorld1 = function (populations) {
+//     return (populations / 7900) * 100;
+// }
 
-const populations = [35, 27, 15, 109];
-// console.log(populations.length);
+// const populations = [35, 27, 15, 109];
+// // console.log(populations.length);
 
-// const perc1 = percentageOfWorld1(populations[0]);
-// const perc2 = percentageOfWorld1(populations[1]);
-// const perc3 = percentageOfWorld1(populations[2]);
-// const perc4 = percentageOfWorld1(populations[3]);
+// // const perc1 = percentageOfWorld1(populations[0]);
+// // const perc2 = percentageOfWorld1(populations[1]);
+// // const perc3 = percentageOfWorld1(populations[2]);
+// // const perc4 = percentageOfWorld1(populations[3]);
 
-// console.log(perc1, perc2, perc3, perc4);
+// // console.log(perc1, perc2, perc3, perc4);
 
-const percentages = [percentageOfWorld1 (populations[0]), percentageOfWorld1 (populations[1]), percentageOfWorld1 (populations[2]), percentageOfWorld1 (populations[3])];
-console.log(percentages);
+// const percentages = [percentageOfWorld1 (populations[0]), percentageOfWorld1 (populations[1]), percentageOfWorld1 (populations[2]), percentageOfWorld1 (populations[3])];
+// console.log(percentages);
+const neighbors = ['Mexico', 'Canada'];
+console.log(neighbors);
+
+neighbors.push ('Utopia');
+console.log(neighbors);
+neighbors.pop();
+console.log(neighbors);
