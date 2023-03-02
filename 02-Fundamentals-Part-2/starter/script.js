d512cbc072312a3644f6034c71fd68fa86b1a551
George Johnson <gjohnson.pioneer@gmail.com>
2023-03-01 19:00:42 -0500

    Add starter files for Part 2 Challenge 1.

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -39,9 +39,25 @@
 // const perUSA1= percentageOfWorld1(332);
 // console.log(percPortugal1, percChina, percPortugal1);
 
-const percentageOfWorld3 = population => (population / 7900 * 100);
-const percPortugal1= percentageOfWorld3(10);
-const percChina= percentageOfWorld3(1441);
-const perUSA1= percentageOfWorld3(332);
-console.log(percPortugal1, percChina, percPortugal1);
+// const percentageOfWorld3 = population => (population / 7900 * 100);
+// const percPortugal1= percentageOfWorld3(10);
+// const percChina= percentageOfWorld3(1441);
+// const perUSA1= percentageOfWorld3(332);
+// console.log(percPortugal1, percChina, percPortugal1);
 
+// function percentageOfWorld1(people) {
+//         return (population / 7900) * 100;
+//     }
+
+function percentageOfWorld1(population) {
+        return (population / 7900) * 100;
+    }
+
+const describePopulation = function (country, population) {
+    const percentage = percentageOfWorld1(population);
+    const description = `${country} has ${population} million people, which is about ${percentage}% of the world`;
+    console.log(description);
+}
+describePopulation('Portugal', 10);
+describePopulation("China", 1441);
+describePopulation("USA", 332);
---
+++ 30-coding-challenge1-part2/script.js
@@ -0,0 +1,23 @@
+// let dolphinsScore = ((96 + 108 + 908) / 3);
+// let koalasScore = ((96 + 108 + 908) / 3);
+
+// console.log(dolphinsScore, koalasScore)
+// if (dolphinsScore > koalasScore && dolphinsScore >= 100){
+//     console.log("Dolphins win! 🏆");
+// } else if (koalasScore > dolphinsScore && koalasScore >= 100){
+//     console.log("Koala's win!!🏆");
+// } else if (dolphinsScore === koalasScore && dolphinsScore >= 100 && koalasScore >= 100){
+//     console.log("It's a draw!")
+// }
+//  else {console.log('There is no winner');}   
+'use strict'
+
+const calcAverage = gameAvg => gameAvg / 3;
+    // console.log(calcAverage);
+function checkWinner(game1, game2, game3) {
+    const dolphinsScores = (44, 23, 71);
+    console.log(checkWinner);
+    
+
+}
+
