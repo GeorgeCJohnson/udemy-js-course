b137647c0e9b161184052d33e6951b8e6f4fb233
George Johnson <gjohnson.pioneer@gmail.com>
2023-03-01 20:55:28 -0500

    Complete challenge 1.

--- 30-coding-challenge1-part2/script.js
+++ 30-coding-challenge1-part2/script.js
@@ -1,15 +1,24 @@
-// let dolphinsScore = ((96 + 108 + 908) / 3);
-// let koalasScore = ((96 + 108 + 908) / 3);
-
-// console.log(dolphinsScore, koalasScore)
-// if (dolphinsScore > koalasScore && dolphinsScore >= 100){
-//     console.log("Dolphins win! 🏆");
-// } else if (koalasScore > dolphinsScore && koalasScore >= 100){
-//     console.log("Koala's win!!🏆");
-// } else if (dolphinsScore === koalasScore && dolphinsScore >= 100 && koalasScore >= 100){
-//     console.log("It's a draw!")
-// }
-//  else {console.log('There is no winner');}   
 'use strict'
 
+const calcAverage = (game1, game2, game3) => (game1 + game2 + game3) / 3;
 
+// Test 1
+// const scoreDolphins = calcAverage(44, 23, 71);
+// const scoreKoalas = calcAverage (65, 54, 49);
+// console.log(scoreDolphins, scoreKoalas);
+
+// Test 2
+const scoreDolphins = calcAverage(85, 54, 41);
+const scoreKoalas = calcAverage (23, 34, 27);
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+    if (avgDolphins >= 2 * avgKoalas){
+    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgKoalas >= 2 * avgDolphins)
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+    else {
+        console.log(`No team wins`)
+    }
+}
+checkWinner(scoreDolphins, scoreKoalas)
