1d563ba888d86d3be10729e218ea2070a264eb09
George Johnson <gjohnson.pioneer@gmail.com>
2023-03-02 11:40:16 -0500

    Complete lesson 39.

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,19 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const percentageOfWorld1 = function (populations) {
+    return (populations / 7900) * 100;
+}
+
+const populations = [35, 27, 15, 109];
+// console.log(populations.length);
+
+// const perc1 = percentageOfWorld1(populations[0]);
+// const perc2 = percentageOfWorld1(populations[1]);
+// const perc3 = percentageOfWorld1(populations[2]);
+// const perc4 = percentageOfWorld1(populations[3]);
+
+// console.log(perc1, perc2, perc3, perc4);
+
+const percentages = [percentageOfWorld1 (populations[0]), percentageOfWorld1 (populations[1]), percentageOfWorld1 (populations[2]), percentageOfWorld1 (populations[3])];
+console.log(percentages);
